f1a5fd9216a76679ba63126a85a6287058c114fe
PepegaGranie <zielinski.kacper@outlook.com>
2024-01-04 17:36:41 +0100

    Update test.h

--- test/test.h
+++ test/test.h
@@ -6,7 +6,7 @@
 using namespace std;
 
 float currentmoneytable(int a){
-map<int,float>currentmoney={{1,0.78},{2,13153.7},{3,75559.8},{4,45864.6},{5,53276.2},{6,21895.7},{7,4704.41},{8,67885.8}};
+map<int,float>currentmoney={{1,0.780029},{2,13153.7},{3,75559.8},{4,45864.6},{5,53276.2},{6,21895.7},{7,4704.41},{8,67885.8}};
 return currentmoney[a];}
 
 #endif // test.h
